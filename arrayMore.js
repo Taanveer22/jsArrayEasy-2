ebb21b9651fb2ae90d304ec70536e07bc35588dd
Taanveer22 <taanveer469@gmail.com>
2025-05-03 02:00:26 +0600

    alhamdulliah array more practice done

---
+++ arrayMore.js
@@ -0,0 +1,40 @@
+
+// rare example 01
+const products = [
+  { phone: "xiaomi", price: 15000, color: "blue" },
+  { phone: "realme", price: 25000, color: "black" },
+  { phone: "nokia", price: 35000, color: "teal" },
+];
+console.log(products);
+console.log(products[0]);
+console.log(products[1]);
+
+
+// rare example 02
+const students = [22, 44, 88, 99];
+console.log(students);
+const boys = {
+  0: 22,
+  1: 44,
+  2: 88,
+  3: 99,
+};
+console.log(boys);
+console.log(boys[0]);
+console.log(boys[1]);
+console.log(boys[2]);
+console.log(boys[3]);
+
+
+// rare example 03
+const laptops = ["asus", "msi", "gigabyte"];
+console.log(laptops);
+const computers = {
+  0: "lenovo",
+  1: "hp",
+  2: "dell",
+};
+console.log(computers);
+console.log(computers[0]);
+console.log(computers[1]);
+console.log(computers[2]);
